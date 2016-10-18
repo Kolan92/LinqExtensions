4f6154ae0a995f279b0bad079b9872566218558b
Pawel Kolanowski <pawel.krzysztof.kolanowski@gmail.com>
2016-10-18 18:14:29 +0200

    Linq Extensions, add GroupAdjecantBy method.

---
+++ LinqExtensions/Extensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExtensions
+{
+    public class GroupOfAdjacent<TSource, TKey> : IEnumerable<TSource>, IGrouping<TKey, TSource> {
+        public TKey Key { get; private set; }
+        private IList<TSource> GroupList { get; set; }
+        public IEnumerator<TSource> GetEnumerator() {
+            return GroupList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+        public GroupOfAdjacent(IList<TSource> source, TKey key) {
+            GroupList = source;
+            Key = key;
+        }
+    }
+
+    public static class Extensions {
+
+        public static IEnumerable<IGrouping<TKey, TSource>> GroupAdjecantBy<TKey, TSource>(
+            this IEnumerable<TSource> collection, Func<TSource, TKey> selector) {
+
+            var lastKey = default(TKey);
+            var isFirst = true;
+            var groupElements = new List<TSource>();
+
+            foreach (TSource element in collection) {
+                var key = selector(element);
+                if (isFirst) {
+                    isFirst = false;
+                    lastKey = key;
+                    groupElements.Add(element);
+                }
+                else {
+                    if (lastKey.Equals(key)) {
+                        groupElements.Add(element);
+                    }
+                    else {
+                        yield return new GroupOfAdjacent<TSource, TKey>(groupElements, lastKey);
+                        groupElements = new List<TSource>();
+                        groupElements.Add(element);
+                        lastKey = key;
+                    }
+                }
+            }
+            if (!isFirst)
+                yield return new GroupOfAdjacent<TSource, TKey>(groupElements, lastKey);
+        }
+    }
+}
---
+++ LinqExtensionsTests/LinqExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using LinqExtensions;
+using NUnit.Framework;
+
+namespace LinqExtensionsTests {
+    public class TestGrouping<TKey, TElement> : List<TElement>, IGrouping<TKey, TElement> {
+        public TKey Key {
+            get;
+            set;
+        }
+
+        public TestGrouping(TKey key, params TElement[] elemetns) {
+            Key = key;
+            this.AddRange(elemetns);
+        }
+
+    }
+    [TestFixture]
+    public class LinqExtensionsTests {
+        [Test]
+        public void Given_Collcetion_Of_Numbers_When_GroupAdjecantBy_Then_Returns_Collection_Of_Adjecant_Groups() {
+            var numbers = new int[] { 1, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0 };
+
+            var groups = numbers.GroupAdjecantBy(i => i);
+            var expectedGropus = new List<IGrouping<int, int>>() {
+                new GroupOfAdjacent<int,int> (new List<int> {1},     1),
+                new GroupOfAdjacent<int,int> (new List<int> {0,0},   0),
+                new GroupOfAdjacent<int,int> (new List<int> {2},     2),
+                new GroupOfAdjacent<int,int> (new List<int> {0,0,0}, 0),
+                new GroupOfAdjacent<int,int> (new List<int> {2},     2),
+                new GroupOfAdjacent<int,int> (new List<int> {0,0,0}, 0)
+            };
+
+            CollectionAssert.AreEqual(expectedGropus, groups);
+        }
+
+        [Test]
+        public void Given_Empty_Collcetion_Of_Numbers_When_GroupAdjecantBy_Then_Returns_Empty_Collection_Of_Adjecant_Groups() {
+            var numbers = new int[0];
+
+            var groups = numbers.GroupAdjecantBy(i => i);
+            CollectionAssert.IsEmpty(groups);
+        }
+    }
+}
